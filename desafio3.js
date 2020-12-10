d01e2c040d8a95b3f37fe3fbe12f1e563390539f
Lara Fernanda <lamorimalvescavalcante@gmail.com>
2020-12-10 08:10:51 -0300

    Primeiros passos com Javascript

---
+++ desafio3.js
@@ -0,0 +1,25 @@
+document.getElementById('div1').innerHTML = "Criado por Lara Fernanda";
+
+document.getElementById('div2').innerHTML = "Acessado em: "+Date();
+
+
+
+document.getElementById('footer1').style.textAlign = 'center';
+
+document.getElementById('footer1').style.marginBottom = '20px';
+
+
+
+document.getElementById('div1').style.backgroundColor = '#7a7a7a';
+
+document.getElementById('div2').style.backgroundColor = '#7a7a7a';
+
+
+
+document.getElementById('div1').style.padding = '0';
+
+document.getElementById('div1').style.marginBottom = '0';
+
+document.getElementById('div2').style.marginTop = '0';
+
+document.getElementById('div2').style.padding = '0';
